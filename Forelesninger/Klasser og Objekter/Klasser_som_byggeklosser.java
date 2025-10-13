1a335c42b46dd281ed0dc54252c45b9617f996cc
SolveigNatvig <solveig.natvig@gmail.com>
2025-10-13 11:11:55 +0200

    Remove Forelesninger from repository

--- Forelesninger/Klasser og Objekter/Klasser_som_byggeklosser.java
+++
@@ -1,4 +0,0 @@
-public class Klasser_som_byggeklosser {
-    
-    
-}
--- Forelesninger/StringManipulering/Time111.java
+++
@@ -1,8 +0,0 @@
-package Forelesninger.StringManipulering;
-
-import java.time.LocalDate;
-import java.time.DayOfWeek;
-
-public class Time111 {
-    DayOfWeek a = LocalDate.now().getDayOfWeek();
-}
